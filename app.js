a7fee345216e4fc936434bc558b1fed9e0253767
ZhraaBaharAluloom <alwarda@hotmail.co.uk>
2020-07-21 22:00:52 +0300

    add input

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const tasks = require("./tasks");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.json(tasks);
+});
+
+app.post("/", (req, res) => {
+  const id = tasks[tasks.length - 1].id + 1;
+  const status = true;
+  const newTask = { id, status, ...req.body };
+  tasks.push(newTask);
+  res.status(201).json(newTask);
 });
 
 app.listen(8000, () => {
